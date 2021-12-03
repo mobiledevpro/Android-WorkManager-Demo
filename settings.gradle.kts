b44a64263d3c2536c6ab29afde9cb9f00659d9a3
Dmitriy Chernysh <dmitriy.chernysh@gmail.com>
2021-12-03 13:32:07 +0200

    Remove extra code

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,7 @@
 include(
     ":app",
-    ":common-ui",
     ":core:navigation",
     ":core:utils",
     ":core:rx",
-    ":core:errors",
-    ":core:database",
-    ":feature:chat_core",
-    ":feature:chat_main",
-    ":feature:profile_settings"
+    ":core:database"
 )
