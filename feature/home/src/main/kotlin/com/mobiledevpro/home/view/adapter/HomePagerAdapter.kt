6b6d88eecc25f1b82a910377dd2f567a147c01f7
Dmitriy Chernysh <dmitriy.chernysh@gmail.com>
2022-01-06 22:38:19 +0200

    Prepared a ViewPager

---
+++ feature/home/src/main/kotlin/com/mobiledevpro/home/view/adapter/HomePagerAdapter.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2021 | Dmitri Chernysh | http://mobile-dev.pro
+ *
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package com.mobiledevpro.home.view.adapter
+
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+/**
+ * ViewPager adapter for Home screen [com.mobiledevpro.home.view.HomeFragment]
+ *
+ * Created on Jan 06, 2022.
+ *
+ */
+class HomePagerAdapter(
+    hostFragment: Fragment
+) : FragmentStateAdapter(hostFragment) {
+
+    override fun getItemCount(): Int = PAGE_COUNT
+
+    override fun createFragment(position: Int): Fragment =
+        when (position) {
+            1 -> Fragment()
+            2 -> Fragment()
+            else -> throw RuntimeException("Fragment not found")
+        }
+
+    companion object {
+        const val PAGE_COUNT = 2
+    }
+
+}
